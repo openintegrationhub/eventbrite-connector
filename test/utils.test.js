dc64553a2e04329d602d1f24c7558c0e1e386cc2
SvenHoeffler <shoeffler@wice.de>
2021-02-15 15:16:44 +0100

    Added general structure

---
+++ test/utils.test.js
@@ -0,0 +1,10 @@
+/* eslint no-unused-expressions: "off" */
+
+const { expect } = require('chai');
+// const { attendeeToOih } = require('../lib/transformations/attendeeToOih');
+
+describe('transformations', () => {
+  it('should be a placeholder test', async () => {
+    expect(true).to.be.true;
+  });
+});
